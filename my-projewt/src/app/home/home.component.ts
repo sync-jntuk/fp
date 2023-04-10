4f323a975293295d82e4988d147ca364f14f2977
sandy-blaze <sandyblaze911@gmail.com>
2023-04-10 12:14:53 +0530

    SC

--- my-projewt/src/app/home/home.component.ts
+++ my-projewt/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BackendService } from '../services/backend/backend.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-home',
@@ -7,9 +9,41 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
-	constructor() { }
+	constructor(private router: Router, private bk: BackendService) { }
 
-	ngOnInit() {
+	queries: Number = 0
+	logins: Number = 0
+	global_queries: Number = 0
+	global_logins: Number = 0
+	notifications: any = []
+	email: any = {}
+
+	ngOnInit(): void {
+		this.email = localStorage.getItem("email")
+		if (!this.email) {
+			this.router.navigateByUrl('/login')
+			return
+		}
+		this.getNotifications()
+	}
+
+	getNotifications() {
+		this.bk.post('/notification/get', { limit: 6 }).subscribe(data => {
+			console.log(data)
+			this.notifications = data
+		})
+	}
+
+	goToHome() {
+		this.router.navigateByUrl('/home')
+	}
+
+	goToNotifications() {
+		this.router.navigateByUrl('/upload-notifications')
+	}
+
+	goToProfile() {
+		this.router.navigateByUrl('/profile')
 	}
 
 }
